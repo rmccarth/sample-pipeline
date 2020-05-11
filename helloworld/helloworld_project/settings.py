0e59c92efa0c128b1429692ce6b7113b879afbef
slixperi <rmccarth@andrew.cmu.edu>
2020-05-11 16:44:03 -0400

    allow all hosts

--- helloworld/helloworld_project/settings.py
+++ helloworld/helloworld_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pwvfemw)dlcy1zg15^ai#tvu+fs=dw30p9+=gpmh!#m%!m!_zk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
